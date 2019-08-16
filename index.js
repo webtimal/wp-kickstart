1d9377a6299ebc0444a712b295e0e36ed9f85a87
eovel <dominik.buechler@webtimal.ch>
2019-08-16 10:44:18 +0200

    Logs added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const { execSync } = require('child_process')
 
 // Dependency modules
-const fs        = require('fs')
-const parseJson = require('parse-json')
+const fs         = require('fs')
+const parseJson  = require('parse-json')
+const logSymbols = require('log-symbols')
 
 // Local modules
 const cmdr = require('./cmdr.js')
@@ -27,9 +28,14 @@ fs.readFile('wp-kickstart.json', (error, buffer) =>
 function run(config)
 {
     // WordPress download
+    console.log(logSymbols.info, 'Downloading WordPress...')
+
     execSync(cmdr.build('wp core download', {
         "locale": config.init.lang
     }))
+
+    console.log(logSymbols.success, 'Download complete')
+
 
     // Dabatase config
     execSync(cmdr.build('wp core config', {
@@ -38,10 +44,20 @@ function run(config)
         "dbpass": config.database.pass
     }))
 
+    console.log(logSymbols.success, 'Main configuration complete')
+
+
     // Database creation
+    console.log(logSymbols.info, 'Creating database')
+
     execSync(cmdr.build('wp db create'))
 
+    console.log(logSymbols.success, 'Database created')
+
+
     // Core installation
+    console.log(logSymbols.info, 'Installing WordPress...')
+
     execSync(cmdr.build('wp core install', {
         "url":            config.init.url,
         "title":          config.init.title,
@@ -51,6 +67,9 @@ function run(config)
         "skip-email":    !config.init.notify
     }))
 
+    console.log(logSymbols.success, 'Installation complete')
+
+
     // Configuration
     for(let [key, value] of Object.entries(config.configs))
     {
@@ -59,31 +78,49 @@ function run(config)
         execSync(cmdr.build(`wp config set ${key} ${value}`, raw))
     }
 
+    console.log(logSymbols.info, 'Custom configurations set')
+
+
     // Themes installation
     for(let [key, value] of Object.entries(config.components.themes))
     {
         let activate = value ? { "activate": true } : {}
 
+        console.log(logSymbols.info, 'Installing theme: ' + key)
+
         execSync(cmdr.build(`wp theme install ${key}`, activate))
+
+        console.log(logSymbols.success, 'Theme ' + key + ' installed')
     }
+
 
     // Plugins installation
     for(let [key, value] of Object.entries(config.components.plugins))
     {
         let activate = value ? { "activate": true } : {}
 
+        console.log(logSymbols.info, 'Installing plugin: ' + key)
+
         execSync(cmdr.build(`wp plugin install ${key}`, activate))
+
+        console.log(logSymbols.success, 'Plugin ' + key + ' installed')
     }
+
 
     // Theme cleanup
     for(let theme of config.cleanup.themes)
     {
         execSync(cmdr.build(`wp theme delete ${theme}`))
+
+        console.log(logSymbols.info, 'Theme ' + theme + ' deleted')
     }
+
 
     // Plugin cleanup
     for(let plugin of config.cleanup.plugins)
     {
         execSync(cmdr.build(`wp plugin delete ${plugin}`))
+
+        console.log(logSymbols.info, 'Plugin ' + plugin + ' deleted')
     }
 }
